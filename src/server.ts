39148a3b5bc400723c46862315d9593ec6236f0f
Hyeon97 <dhdhgustlr@naver.com>
2025-07-28 08:50:41 +0000

    .

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,7 @@ app.use(express.json())
 
 // GET "/" 엔드포인트
 app.get('/', (req: Request, res: Response) => {
-  res.send('Server is Running in NKS (1).')
+  res.send('Server is Running in NKS (2).')
 })
 
 // 서버 시작
